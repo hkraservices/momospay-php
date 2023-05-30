949246772efb05a21e4480b8aba230bc8a3a2b2b
Kolawole H <krochdan@gmail.com>
2023-05-30 10:30:38 +0100

    fix:Retrieve Transactions

--- sample/index.php
+++ sample/index.php
@@ -5,35 +5,38 @@ use Momospay\Transaction;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$secret_key = "hgdghvdfdvfdvfdbdvvdvfdvfvfvfbbbdbdbbd";
+$secret_key = "sk_1_Y7H1cL0OlIM7d89YTNmZNViIPgDK376s5d45dsf";
 
 Momospay::setApiKey($secret_key);
 Momospay::setEnvironment("sandbox");
 
-// $transaction = Transaction::create([
-//     'transaction' => [
-//         'amount' => "100",
-//         'description' => 'Paiement trajet' . "sdsdssds",
-//     ],
-//     'customer' => [
-//         'email' => "sdsdsd",
-//         'lastname' => 'dsdd',
-//         'firstname' =>  'ddffdf'
-//     ],
-//     'custom_metadata' => [
-//         'Jaque' => 'jdf,nfnd,fdnf',
-//         "roun" => 'ndfjnjnfnff'
-//     ],
-//     'return_url' => null,
-//     'site_domains' => 'comotoltd.com',
-//     'methodepayment' => 'mobile',
-// ]);
-
-$transaction = Transaction::retrieve(1);
+$transaction = Transaction::create([
+    'transaction' => [
+        'amount' => "100",
+        'description' => 'Paiement trajet' . "sdsdssds",
+    ],
+    'customer' => [
+        'email' => "sdsdsd",
+        'lastname' => 'dsdd',
+        'firstname' =>  'ddffdf'
+    ],
+    'custom_metadata' => [
+        'Jaque' => 'jdf,nfnd,fdnf',
+        "roun" => 'ndfjnjnfnff'
+    ],
+    'return_url' => null,
+    'message_success' => null,
+    'site_domains' => 'comotoltd.com',
+    'methodepayment' => 'mobile',
+]);
 
 if ($transaction) {
     var_dump($transaction);
 }
+
+$transaction_ = Transaction::retrieve(4);
+var_dump($transaction_);
+
 
 
 // var_dump($transaction);
