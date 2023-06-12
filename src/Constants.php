e7872aae014cd1da16eda415d590accb7de58029
Kolawole H <krochdan@gmail.com>
2023-06-12 15:07:11 +0100

    feat: job mail

--- src/Constants.php
+++ src/Constants.php
@@ -5,8 +5,8 @@ namespace Momospay;
 final class Constants
 {
      const LOCAL_URL                    = "http://127.0.0.1:8000/";
-     const SANDBOX_URL                  = "https://sandbox.api.momospay.com/";
+     const SANDBOX_URL                  = "https://api-sandbox.momospay.com/";
      const LIVE_URL                     = "https://api.momospay.com/";
-     const FRONT_BASE_URL_SANDBOX       = "https://sandbox.front.momospay.com/";
+     const FRONT_BASE_URL_SANDBOX       = "https://front-sandbox.momospay.com/";
      const FRONT_BASE_URL_LIVE          = "https://front.momospay.com/";
 }
